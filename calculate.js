f2af9a4e9bc9fe3cef39ab8e2dc8145f2b89bcc3
rafa <rafaelb@unicauca.edu.co>
2020-06-13 11:28:27 -0500

    FIX: Unidades formula 2,(creo que aun hay un problema en la formula, por eso dividi entre 1000  L2)

--- calculate.js
+++ calculate.js
@@ -5,16 +5,15 @@ const c1 = 1.07;
 const c2 = 2.29;
 const c3 = 0;
 const c4 = 3.55;
-const permeabilidad = 1.256637061 / 1000000;
+const permeabilidad = 1.256637061 * Math.pow(10, -6);
 
 // Formula 2
-const beta = 1.33 / 1000;
+const beta = 1.33 * Math.pow(10, -3);
 const a1 = -1.21;
 const a2 = -0.163;
 const a3 = 2.43;
 const a4 = 1.75;
 const a5 = -0.049;
-const micro = 1000000;
 console.log(beta, a1, a2, a3, a4, a5);
 
 var inductor = document.getElementById("result");
@@ -25,12 +24,13 @@ var inductor2 = document.getElementById("result2");
 var dout = document.getElementById("myRange");
 var d_outText = document.getElementById("d_out");
 var z_in = document.getElementById("impedance");
+var z_in_2 = document.getElementById("impedance2");
 d_outText.innerHTML = dout.value; // Display the default slider value
 
-function davgFinal() {
+function davgFinal(out, din) {
   //   var numerador = parseFloat(dout.value) - parseFloat(din.value);
   //   var denomidador = parseFloat(dout.value) + parseFloat(din.value);
-  return (parseFloat(dout.value / micro) + parseFloat(din.value / micro)) * 0.5;
+  return (parseFloat(out) + parseFloat(din)) * 0.5;
 }
 
 // Update the current slider value (each time you drag the slider handle)
@@ -96,33 +96,45 @@ function nVueltas(vueltas) {
 }
 
 function calcInductor() {
-  let d_out = parseFloat(dout.value / micro);
-  let d_in = parseFloat(din.value / micro);
-  let width = parseFloat(w.value / micro);
+  let d_out = parseFloat(dout.value * Math.pow(10,-6));
+  let d_in = parseFloat(din.value * Math.pow(10,-6));
+  let width = parseFloat(w.value * Math.pow(10,-6));
   let vueltas = nVueltas(parseFloat(n.value));
-  let spacing = parseFloat(s.value / micro);
+  let spacing = parseFloat(s.value * Math.pow(10,-6));
   let freq = parseFloat(f.value / 10);
-  let omega = 2 * Math.PI * freq * 1000000000;
+  let omega = 2 * Math.PI * freq * Math.pow(10,9);
   let p = (d_out - d_in) / (d_out + d_in);
-  var L =
+  console.log(`d_out:`, d_out);
+  console.log(`d_in:`, d_in);
+  console.log(`width:`, width);
+  console.log(`vueltas:`, vueltas);
+  console.log(`spacing:`, spacing);
+  console.log(`freq:`, freq);
+  console.log(`w:`, omega);
+  console.log(`p,fill relation:`, p);
+  console.log(`d_avg:`, davgFinal(d_out, d_in));
+  
+  var L1 =
+    0.5 *
+    permeabilidad *
+    Math.pow(vueltas, 2) *
+    davgFinal(d_out, d_in) *
+    c1 *
+    (Math.log(c2 / p) + c3 * Math.pow(p, 2));
+  inductor2.innerHTML = L1 * Math.pow(10, 9);
+  console.log(L1);
+  z_in.innerHTML = L1 * omega;
+
+  var L2 =
     beta *
     Math.pow(d_out, a1) *
     Math.pow(width, a2) *
-    Math.pow(davgFinal(), a3) *
+    Math.pow(davgFinal(d_out, d_in), a3) *
     Math.pow(vueltas, a4) *
     Math.pow(spacing, a5);
-  // inductor.innerHTML = L*Math.pow(10,9);
-  
-  var L2 =
-  0.5 *
-  permeabilidad *
-  Math.pow(vueltas, 2) *
-  davgFinal() *
-  c1 *
-  (Math.log(c2 / p) + c3 * Math.pow(p, 2));
-  inductor2.innerHTML = L2*Math.pow(10,9)
-  console.log(L2);
-  z_in.innerHTML = L2 * omega;
+  inductor.innerHTML = (L2 * Math.pow(10, 9)) / 1000;
+  z_in_2.innerHTML = L2*omega/1000
+
 }
 // SLIDER s
 // var slider_s = document.getElementById("mydin");
